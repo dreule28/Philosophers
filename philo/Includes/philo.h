b90506a14fefbdb058377baa254deec11708d4e5
Daniel Reule <dreule@1-E-7.42heilbronn.de>
2025-02-13 15:45:50 +0100

    Initial commit. set up structure, Makefile working.

---
+++ philo/Includes/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/13 13:44:02 by dreule            #+#    #+#             */
+/*   Updated: 2025/02/13 13:47:15 by dreule           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+//Includes -- BEGIN
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+//Includes -- END
+
+#endif
