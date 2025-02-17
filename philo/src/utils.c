ba15c8119e1cf8fee6c1911bd2d089a5d6b981af
Daniel Reule <dreule@2-I-1.42heilbronn.de>
2025-02-17 16:18:18 +0100

    NEW(utils.c): contains atoi, strlen, erro_init

---
+++ philo/src/utils.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/17 14:20:19 by dreule            #+#    #+#             */
+/*   Updated: 2025/02/17 15:53:56 by dreule           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int	min_count;
+	int	i;
+	int	nb;
+
+	i = 0;
+	min_count = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			min_count++;
+		i++;
+	}
+	nb = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = nb * 10 + (str[i] - '0');
+		i++;
+	}
+	if (min_count % 2 != 0)
+		nb = -nb;
+	return (nb);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	error_init(char *str)
+{
+	write(2, str, ft_strlen(str));
+}
