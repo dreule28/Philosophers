20c737ee057cd54c3ab0d07f68f5d692c67f9cb8
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-02-19 13:53:44 +0100

    ADD(philo.h): added structs containg forks etc. and doubly circular linked list as table. added fucntion prototypes

--- philo/Includes/philo.h
+++ philo/Includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 13:44:02 by dreule            #+#    #+#             */
-/*   Updated: 2025/02/13 13:47:15 by dreule           ###   ########.fr       */
+/*   Updated: 2025/02/19 13:52:00 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,45 @@
 # include <string.h>
 # include <sys/time.h>
 # include <pthread.h>
+# include <stdbool.h>
 //Includes -- END
+typedef struct s_shared
+{
+	int				nb_of_philos;
+	time_t			time_to_die;
+	time_t			time_to_eat;
+	time_t			time_to_sleep;
+	time_t			sim_start;
+	int				nb_of_meals;
+	pthread_mutex_t	mutex;
+	struct s_philo	*table;
+}	t_shared;
+
+typedef struct s_philo
+{
+	pthread_t		thread_id;
+	int				philo_id;
+	int				times_eaten;
+	time_t			time_last_meal;
+	pthread_mutex_t	*r_fork_mutex;
+	pthread_mutex_t	l_fork_mutex;
+	struct s_data	*data;
+	struct s_philo	*right;
+	struct s_philo	*left;
+}	t_philo;
+
+//Functions -- BEGIN
+bool	check_args_and_init(char **argv, t_shared *data);
+bool	is_not_nb(char *argv);
+int		ft_atoi(const char *str);
+void	error_init(char *str);
+int		ft_strlen(char *str);
+time_t	get_time_ms(void);
+t_philo	*set_table(t_shared *data);
+void	get_right_fork(t_philo *table, int nb_of_philos);
+t_philo	*place_chair_at_table(t_philo *new_chair,
+			t_philo *table, t_philo *last);
+t_philo	*add_chair(t_shared *data, int i);
+//Functions -- END
 
 #endif
