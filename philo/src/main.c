5c187c7f04845ccc91b3965be274f8d90ac163b8
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-02-19 11:25:18 +0100

    ADD(main.c): added error handling and assignment of args

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 14:37:57 by dreule            #+#    #+#             */
-/*   Updated: 2025/02/17 16:17:20 by dreule           ###   ########.fr       */
+/*   Updated: 2025/02/19 11:22:07 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,27 +26,45 @@ bool	is_not_nb(char *argv)
 	return (false);
 }
 
-int	check_args(char **argv)
+int	init_args(char *argv, t_shared *data)
 {
-	if (ft_atoi(argv[1]) > PHILO_MAX || ft_atoi(argv[1]) <= 0 || is_not_nb(argv[1]))
-		return (error_init("Invalid number_of_philosophers!\n"), 0);
+	data->nb_of_philos = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	if (argv[5])
+		data->nb_of_meals = ft_atoi(argv[5]);
+	else
+		data->nb_of_meals = 0;
+	data->sim_start = get_time_ms();
+}
+
+bool	check_args_and_init(char **argv, t_shared *data)
+{
+	if (ft_atoi(argv[1]) > 200 || ft_atoi(argv[1]) <= 0 || is_not_nb(argv[1]))
+		return (error_init("Invalid number_of_philosophers!\n"), false);
 	else if (ft_atoi(argv[2]) <= 0 || is_not_nb(argv[2]))
-		return (error_init("Invalid time_to_die!\n"), 0);
+		return (error_init("Invalid time_to_die!\n"), false);
 	if (ft_atoi(argv[3]) <= 0 || is_not_nb(argv[3]))
-		return (error_init("Invalid time_to_eat!\n"), 0);
+		return (error_init("Invalid time_to_eat!\n"), false);
 	if (ft_atoi(argv[4]) <= 0 || is_not_nb(argv[4]))
-		return (error_init("Invalid time_to_sleep!\n"), 0);
+		return (error_init("Invalid time_to_sleep!\n"), false);
 	if (argv[5] && (ft_atoi(argv[5]) <= 0 || is_not_nb(argv[5])))
-		return (error_init("Invalid number_of__times_each_philosopher_must_eat!\n"), 0);
-	return (1);
+		return (error_init("Invalid number_of_meals!\n"), false);
+	if (!init_args(argv, data))
+		return (false);
+	return (true);
 }
 
 int	main(int argc, char **argv)
 {
+	t_shared	data;
+
 	if (argc < 5 || argc > 6)
 		return (printf("Too many or too few Arguments!\n"), 1);
-	if (!check_args(argv))
+	if (!check_args_and_init(argv, &data))
 		return (1);
 }
 
 // dorker valgrind --tool=hellgrind
+// test values 5 800 200 200 7
