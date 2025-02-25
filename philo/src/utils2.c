fa41ddb78a7224ed9aceeedb9f302e4c2a7fbdd1
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-02-25 13:58:54 +0100

    FEAT(utils2.c): added edge case when theres only on philo at the table

--- philo/src/utils2.c
+++ philo/src/utils2.c
@@ -6,7 +6,7 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 13:46:11 by dreule            #+#    #+#             */
-/*   Updated: 2025/02/25 13:50:01 by dreule           ###   ########.fr       */
+/*   Updated: 2025/02/25 13:57:55 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,15 @@ void	log_action(t_shared *data, int philo_id, char *log_message)
 	if (!data->sim_stop)
 		printf("%ld %d %s\n", data->sim_start, philo_id, log_message);
 	pthread_mutex_unlock(&data->log_mutex);
+}
+
+void	handle_one_philosopher(t_shared *data, t_philo *philo, int left_fork,
+								int right_fork)
+{
+	pthread_mutex_lock(&data->fork_mutexes[left_fork]);
+	log_action(data, philo->philo_id, "has taken a fork");
+	usleep(data->time_to_die * 1000);
+	log_action(data, philo->philo_id, "died");
+	pthread_mutex_unlock(&data->fork_mutexes[left_fork]);
+	return (NULL);
 }
