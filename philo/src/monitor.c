f0aa31d96f903c25fa453b215e129454bee650aa
dreule <danielreule28@gmail.com>
2025-05-13 19:29:05 +0200

    FEAT/CHORE(monitor.c): simplify monitoring logic and improve readability

--- philo/src/monitor.c
+++ philo/src/monitor.c
@@ -6,58 +6,73 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 15:20:19 by dreule            #+#    #+#             */
-/*   Updated: 2025/03/07 17:36:12 by dreule           ###   ########.fr       */
+/*   Updated: 2025/05/13 19:26:10 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	create_and_join_monitor(t_shared *data)
+bool	has_eaten_enough(t_shared *data, int philo_index)
 {
-	if (pthread_create(&data->monitor, NULL, &monitor_routine, data))
+	return (data->nb_of_meals > 0
+		&& data->philosophers[philo_index].times_eaten >= data->nb_of_meals);
+}
+
+bool	check_philosopher_death(t_shared *data, int philo_i)
+{
+	long	now ;
+
+	now = get_time_ms();
+	if (now - data->philosophers[philo_i].time_last_meal > data->time_to_die)
 	{
-		printf("Error creating monitor!");
-		cleanup_threads(data, data->nb_of_philos);
-		return ;
+		log_action(data, data->philosophers[philo_i].philo_id, "died");
+		set_simulation_stop(data);
+		return (true);
 	}
-	if (pthread_join(data->monitor, NULL))
+	return (false);
+}
+
+bool	all_philosophers_done_eating(t_shared *data, int done_count)
+{
+	if (data->nb_of_meals > 0 && done_count == data->nb_of_philos)
 	{
-		printf("Error joining monitor!");
-		return ;
+		set_simulation_stop(data);
+		return (true);
 	}
+	return (false);
+}
+
+void	set_simulation_stop(t_shared *data)
+{
+	pthread_mutex_lock(&data->stop_mutex);
+	data->sim_stop = true;
+	pthread_mutex_unlock(&data->stop_mutex);
 }
 
 void	*monitor_routine(void *arg)
 {
 	t_shared	*data;
 	int			i;
-	long		curr_time;
+	int			done;
+
 	data = (t_shared *)arg;
 	while (!simulation_stopped(data))
 	{
+		done = 0;
 		i = 0;
 		while (i < data->nb_of_philos)
 		{
 			pthread_mutex_lock(&data->status_mutex);
-			curr_time = get_time_ms();
-			if (curr_time - data->philosophers[i].time_last_meal > data->time_to_die)
-			{
-				pthread_mutex_lock(&data->stop_mutex);
-				if (!data->sim_stop)
-				{
-					data->sim_stop = 1;
-					pthread_mutex_unlock(&data->stop_mutex);
-					log_action(data, data->philosophers[i].philo_id, "died");
-				}
-				else
-					pthread_mutex_unlock(&data->stop_mutex);
-				pthread_mutex_unlock(&data->status_mutex);
-				return (NULL);
-			}
+			if (has_eaten_enough(data, i))
+				done++;
+			else if (check_philosopher_death(data, i))
+				return (pthread_mutex_unlock(&data->status_mutex), NULL);
 			pthread_mutex_unlock(&data->status_mutex);
 			i++;
 		}
-		usleep(1000);
+		if (all_philosophers_done_eating(data, done))
+			return (NULL);
+		usleep(200);
 	}
 	return (NULL);
 }
