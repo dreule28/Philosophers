30bff5b315939a554bbd8ad67925b4d317c39d89
dreule <danielreule28@gmail.com>
2025-05-13 18:57:28 +0200

    FEAT(dining_routine.c): implement dining routine for philosophers with fork management and timing into seperate file

---
+++ philo/src/dining_routine.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dining_routine.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 18:22:38 by dreule            #+#    #+#             */
+/*   Updated: 2025/05/13 18:49:45 by dreule           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	handle_initial_timing(t_shared *data, t_philo *philo)
+{
+	if (philo->philo_id % 2 != 0)
+		custom_sleep(data, data->time_to_eat);
+	if (philo->philo_id % 2 == 0)
+		usleep(200);
+	else
+		usleep(0);
+}
+
+void	release_forks(t_shared *data, int left_fork, int right_fork)
+{
+	pthread_mutex_unlock(&data->fork_mutexes[left_fork]);
+	pthread_mutex_unlock(&data->fork_mutexes[right_fork]);
+}
+
+bool	philo_cycle(t_shared *data, t_philo *philo, int l_fork, int r_fork)
+{
+	log_action(data, philo->philo_id, "is thinking");
+	if (!chose_forks(data, philo, l_fork, r_fork))
+		return (false);
+	if (!philo_eats(data, philo))
+	{
+		release_forks(data, l_fork, r_fork);
+		return (false);
+	}
+	release_forks(data, l_fork, r_fork);
+	if (!philo_sleeps(data, philo))
+		return (false);
+	if (data->time_to_sleep < data->time_to_eat)
+		custom_sleep(data, data->time_to_eat - data->time_to_sleep);
+	return (true);
+}
+
+void	*dining_routine(void *arg)
+{
+	t_philo		*philo;
+	t_shared	*data;
+	int			left_fork;
+	int			right_fork;
+
+	philo = (t_philo *)arg;
+	data = philo->data;
+	left_fork = philo->philo_id - 1;
+	right_fork = philo->philo_id % data->nb_of_philos;
+	handle_initial_timing(data, philo);
+	while (!simulation_stopped(data))
+	{
+		if (!philo_cycle(data, philo, left_fork, right_fork))
+			break ;
+	}
+	return (NULL);
+}
